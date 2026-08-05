eb869d1d1c52579ecadf6052ff8d01dd97abca4c
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2026-08-06 01:00:59 +0530

    arithmetic operator in cpp

---
+++ Day01/arithmeticoperators.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a=5;
+    cout<<a<<endl;
+    cout<<a++<<endl; // cout<<++a; output = 6 and cout<<a++; output = 5
+    cout<<a<<endl; // output = 6
+    // int b=10;
+    // cout<<a+b<<endl;
+    // cout<<a-b<<endl;
+    // cout<<a*b<<endl;
+    // cout<<a/b<<endl; // / special operator for division return only integer value like 5/2=2.5 but it will return 2 only.
+/*
+    // int c;
+    // c =a/b;
+    // cout<<c<<endl;
+    // a++; // Post-increment operator 
+    // cout<<a<<endl;
+    // b--; // Post-decrement operator b = b-1;
+    // cout<<b<<endl;
+    // ++a; // Pre-increment operator a =a+1;
+    // cout<<a<<endl;
+    // --b; // Pre-decrement operator
+    // cout<<b<<endl;
+    */
+    return 0;
+}
