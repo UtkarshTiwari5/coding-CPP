302c6eb72be91204d97cf33c5effa63053a7296a
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2026-08-06 00:58:57 +0530

    boolean data type in cpp

---
+++ Day01/booleanDataType.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // bool flag = true;
+    // cout<<flag;
+    // cout<<(5>2); // 5<2 output is o and 5>2 output is 1; 5>=2 output 1. when 7==4 output 0. and 7==7 output 1;
+    // int x = 4;
+    // int y =5;
+    // cout<<(x==y); when using namespace std not use before cout use std :: cout<<flag;
+
+    float a =5, b=2;
+    int c;
+    // c = a % b; error show
+    cout<<c;  // cout<<2/7 it give 0 output only return integer values. and when cout<<2/7.0 it gives 0.2857.
+
+
+} //error show;
