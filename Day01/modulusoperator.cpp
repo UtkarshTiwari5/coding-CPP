6c6ab0ed5048d4ca94346b9584510f8950385d59
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2026-08-06 01:01:56 +0530

    modulus operator in cpp

---
+++ Day01/modulusoperator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+ int main(){
+//     int a = 7;
+//     int b = 4;
+//int remainder = a%b; // a is dividend and b is divisor
+// if a>b and a=b then remainder = a%b
+//while if a<b then remainder = a
+//     cout<<a%b; // Print the remainder.
+       cout<<100%4<<endl;
+       cout<<-99%4<<endl;
+       cout<<99%-4<<endl;
+       cout<<-99%-4<<endl;
+}
+
+// properties of modulus operator
+
+// if a<b then a%b = a .******
+//a%b = a %(-b)
+// (-a)%b = -(a%b)
+//(-a)%(-b) =  (-a) % b = -(a%b)
