5a81338219597306440852749b9f073ea8951a64
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2026-08-06 01:04:14 +0530

    calculate percentage in cpp

---
+++ Day01/calculatepercentage.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float maths = 90;
+    float physics = 80;
+    float chemistry = 70;
+    float biology = 60;
+    float hindi  =60;
+    float percent = (maths+physics+chemistry+biology+hindi) /5;
+    cout<< percent;
+}
