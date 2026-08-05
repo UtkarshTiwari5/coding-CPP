0086934e1634adb56b5ea24cc73d83f1c3cd7557
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2026-08-06 00:58:26 +0530

    float data type in cpp

---
+++ Day01/floatdatatype.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+ int main() {
+    // float a =5;
+    // float b =2;
+    // cout<<a/b;
+    float d =2/7;  // gives output zero and when cout<<2/7.0 output=0.2857
+    cout<<d;
+    // float a=5.5;
+    // a =a +1;
+    // cout<<a<<endl;
+    //  cout<<5.0;
+    //  cout<<5+3.14;
+    //  cout<<5.0/2.0<<endl;
+    //  cout<<5/2<<endl; // / 5/2 = 2.5 but it will return 2 only because it is integer division.
+    //  cout<<5.0/2<<endl;
+    //  cout<<5/2.0<<endl;
+    //  return 0;
+
+
+// Take two input inegers input ,a and b:a>b, and find the remainder when a is divided by b.
+
+
+// int a = 10;
+// int b = 3;
+// cout<<a%b;  // Dividend = Divisor * Quotient + Remainder. a = b *q +r.
+ }
