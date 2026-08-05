5fcd4a2558270c6c878a6b78c068f2764d1c9b3d
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2026-08-06 00:57:40 +0530

    naming rules in cpp

---
+++ Day01/namingrules.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main (){
+    // int decode = 4 ;
+    // int float1 = 4;
+    // a,b,c,d,A B G H , _ ,$ allowed.
+    // ! @ # % ^ & * ( ) - + = /  |  \ :  ; ' " "  are not allowed.
+    int _R=4; // $R also start variable names.
+    // cout<<float1;
+    cout<<_R;
+
+}
+
+
+// variables naming rules 
+// variables  can start from an alphabet or underscorec_ or $.
+//special characters excepts _ or $ are not allowed.
+// some particular keywords are not allowed .
+// commas or blanks are not allowed .
+// starting number not allowed it means like 1x is not allowed.
+
