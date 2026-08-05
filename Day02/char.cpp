643d10340fdd3702ac4901a107887281300759fa
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2026-08-06 00:55:09 +0530

    About character in cpp

---
+++ Day02/char.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // char ch = 'a';
+    // cout<<ch;
+    // char ch = '1';
+    // char x = 'q';
+    // cout<<ch<<" "<<x;
+    char ch ='A';
+    cout<<(int)ch; // each character has an ASCII Value.
+    // '#' = 35
+    // 'a' = 97
+    // 'A' = 65
+    // '0' = 48
+
+}
