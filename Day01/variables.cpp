89a9128af8b443001c2006bb481a1a82266b937a
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2026-08-06 00:50:11 +0530

    variables in CPP

---
+++ Day01/variables.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int x =19;
+    // int x;
+    // x=19;
+    cout<<x<<endl;
+    x = x +10; // x+=10;
+    cout<<x<<endl;
+    x =x - 20; // x-=20;
+    cout<<x;
+    x = x * 2; // x*=2;
+    // // cout<<x<<endl;
+    // x  = 10; // updating the value of x to 10
+    // cout<<x;
+    return 0;
+}
