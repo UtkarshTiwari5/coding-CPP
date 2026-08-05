771304db4b99146ed9dc36aca4b08157d800aa50
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2026-08-06 01:03:46 +0530

    area of cicle in cpp

---
+++ Day01/areaofcircle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main () {
+    // float r = 8;
+    // float a;
+    // float pi =3.1412;
+    // a =3.1415 *r*r; // float a =3.1412*r*r;
+    // cout<<a;
+    // cout<<3.1412*r*r;
+    float radius;
+    cout<<"enter radius :";
+    cin>>radius;
+    float area = 3.1415*radius*radius;
+    cout<<"area is : ";
+    cout<<area<<endl;
+}
+
+
+
