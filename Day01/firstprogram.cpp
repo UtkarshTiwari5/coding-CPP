2c840cccb9ebb6c540a39549ce61080d3b11f9cc
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2026-08-05 22:01:28 +0530

    Basic program on cpp

---
+++ Day01/firstprogram.cpp
@@ -0,0 +1,25 @@
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     cout << "Hello World"; //or <<endl<<hello;
+//     cout << endl;
+//     cout<< "\n"; // endl to move to the next line and flush the output buffer.
+//     cout<< "Hello World";
+//     return 0;
+// }
+// endl to move to the next line and flush the output buffer.
+
+// cout<<"\n"; or cout<<endl; it is a escape sequence that moves the cursor to the next line. The difference is that endl also flushes the output buffer, while "\n" does not.
+// cout<<"hello\n\n";
+
+// predict the output.
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"nn\n\nnn\n";
+    cout<<"nn/n/nnn/n";
+    return 0;
+}
+
+       
